5fac538e2e084ffd8ed42dabc8fbe2b200aa7b60
Ksusha Lu <angelinalukoperova962@gmail.com>
2021-10-02 14:02:36 +0300

    add skill - if/else

---
+++ skill.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main(){
+    char answer;
+    cout<<"are you sure you want this skill?(y/n) \n";
+    cin >> answer;
+    if(answer=='y'){
+        cout<<"!!!\n";
+    }
+    else if(answer=='n'){
+        cout<<"chose another skill\n";
+    }
+    else{
+        cout<<"error, invalid data\n";
+    }
+    return 0;
+}
