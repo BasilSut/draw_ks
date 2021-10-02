9580a77f8e98ccf50e3eff721ccb2afa25a77e87
Ksusha Lu <angelinalukoperova962@gmail.com>
2021-10-02 14:59:04 +0300

    add example.cpp, hello world

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;//имя endl хранится в пространстве имен std которое хранится в библеотеке iostream подключенной в первой строке
+
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
